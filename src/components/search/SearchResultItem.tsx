ce581b36c4546ec7d60b2bd7b9b43a7379a3dccf
Patryk <dev@pwesolowski.pl>
2026-03-04 17:34:50 +0100

    fix: correct StarRating import in SearchResultItem

--- src/components/search/SearchResultItem.tsx
+++ src/components/search/SearchResultItem.tsx
@@ -1,4 +1,4 @@
-import { StarRating } from '@/components/StarRating'
+import StarRating from '@/components/StarRating'
 import CategoryBadge from '@/components/CategoryBadge'
 import type { SearchResult } from '@/data/search'
 
